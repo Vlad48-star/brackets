c5bd5aca06b1b0a973f168b50dd0f87f48ba19f7
Vlad48-star <85586492+Vlad48-star@users.noreply.github.com>
2021-08-28 19:43:13 +0300

    add:Create my project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ module.exports = function check(str, brakConfig) {
+  let brackets = str;
+  let count = 0;
+
+  while (brackets) {
+    for (let i = 0; i < brakConfig.length; i++) {
+      brackets = brackets.split(brakConfig[i].join("")).join("");
+    }
+
+    count++;
+    if (count > str.length / 2) return false;
+  }
+
+  return true;
+};
